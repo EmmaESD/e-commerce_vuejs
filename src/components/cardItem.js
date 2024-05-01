469c67abb86ab884a2cb348e9b5d5aadb077a338
vaysse emma <emma.vaysse.bourgeois@gmail.com>
2024-05-01 17:43:43 +0200

    create template of card + files style scss

--- src/components/cardItem.js
+++ src/components/cardItem.js
@@ -1,15 +1,25 @@
 export const cardItem = {
+  props: {
+    id: Number,
+    image_url: String,
+    price: Number,
+  },
   template: `
-    <div class="card">
-        <div class="price"></div>
-        <div class="icons">
-            <div class="icon-like">
-                <font-awesome-icon :icon="['far', 'heart']" />
+        <div class="card-container" :key="id">
+            <div class="card-image-price">
+                <div class="price">
+                    <h2>{{price}}</h2>
+                </div>
+                <img :src="image_url" alt="" />
             </div>
-            <div class="icon-shop">
-                <font-awesome-icon :icon="['fas', 'cart-shopping']" />
+            <div class="card-bottom">
+                <div class="like-container">
+                    <i class="fa-regular fa-heart" style="color: #ffffff"></i>
+                </div>
+                <div class="shop-container">
+                    <i class="fa-solid fa-cart-shopping" style="color: #ffffff"></i>
+                </div>
             </div>
         </div>
-    </div>
     `,
 };
--- src/script.js
+++ src/script.js
@@ -1,2 +1,22 @@
 const { createApp, ref } = Vue;
-i;
+import { cardItem } from "./components/cardItem";
+
+const vm = createApp({
+  components: {
+    "card-item": cardItem,
+  },
+
+  data() {
+    return {
+      postList: [],
+    };
+  },
+
+  mounted() {
+    fetch("data.json")
+      .then((response) => response.json())
+      .then((data) => {
+        this.postList = data;
+      });
+  },
+}).mount("#app");
