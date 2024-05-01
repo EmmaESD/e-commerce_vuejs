094cf6f6c604f94ef792cbb187dfe729349f4b8b
vaysse emma <emma.vaysse.bourgeois@gmail.com>
2024-05-01 20:39:25 +0200

    add style

--- src/components/cardItem.js
+++ src/components/cardItem.js
@@ -1,24 +1,25 @@
 export const cardItem = {
   props: {
-    id: Number,
+    index: Number,
     image_url: String,
     price: Number,
+    name: String,
+    like: Boolean,
+    number: Number,
   },
   template: `
-        <div class="card-container" :key="id">
-            <div class="card-image-price">
-                <div class="price">
-                    <h2>{{price}}</h2>
-                </div>
-                <img :src="image_url" alt="" />
+        <div class="card-image-price">
+            <div class="price">
+                <h2>{{price}}</h2>
             </div>
-            <div class="card-bottom">
-                <div class="like-container">
-                    <i class="fa-regular fa-heart" style="color: #ffffff"></i>
-                </div>
-                <div class="shop-container">
-                    <i class="fa-solid fa-cart-shopping" style="color: #ffffff"></i>
-                </div>
+            <img :src="image_url" alt="" />
+        </div>
+        <div class="card-bottom">
+            <div class="like-container">
+                <i class="fa-regular fa-heart" style="color: #ffffff"></i>
+            </div>
+            <div class="shop-container">
+                <i class="fa-solid fa-cart-shopping" style="color: #ffffff"></i>
             </div>
         </div>
     `,
--- src/script.js
+++ src/script.js
@@ -1,9 +1,8 @@
-const { createApp, ref } = Vue;
-import { cardItem } from "./components/cardItem";
+import { cardItem } from "./components/cardItem.js";
 
-const vm = createApp({
+const vm = Vue.createApp({
   components: {
-    "card-item": cardItem,
+    cardItem: cardItem,
   },
 
   data() {
@@ -12,11 +11,17 @@ const vm = createApp({
     };
   },
 
-  mounted() {
-    fetch("data.json")
-      .then((response) => response.json())
-      .then((data) => {
-        this.postList = data;
-      });
+  methods: {
+    showData() {
+      fetch("products.json")
+        .then((response) => response.json())
+        .then((data) => {
+          this.postList = data;
+        });
+    },
+  },
+
+  created() {
+    this.showData();
   },
 }).mount("#app");
